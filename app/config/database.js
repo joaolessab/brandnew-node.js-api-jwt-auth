63841526b8d66413f71a2d8c4009fa5aa2ee2531
João Vitor Lessa <joaovitorlessa@gmail.com>
2022-10-21 15:32:03 -0300

    mongoose library removing parameters not used anymore

--- app/config/database.js
+++ app/config/database.js
@@ -5,12 +5,7 @@ const { MONGO_URI } = process.env;
 exports.connect = () => {
   // Connecting to the database
   mongoose
-    .connect(MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    })
+    .connect(MONGO_URI, {})
     .then(() => {
       console.log("Successfully connected to database");
     })
